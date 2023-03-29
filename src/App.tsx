51284b1d189850ddced087c63547b6145b41b3a0
mikailaliyev <mikail.aliyev@gmail.com>
2023-03-29 23:43:16 +0400

    added favicon and updated project files

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
-import { useState } from "react";
 import "./App.css";
+import { Footer } from "./components/Footer";
+import { Header } from "./components/Header";
+import { Main } from "./components/Main";
 
 function App() {
-  const [count, setCount] = useState(0);
 
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <Header />
+      <Main />
+      <Footer />
     </div>
   );
 }
