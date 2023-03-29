84cd8358174f551962cddd01cc02dd475f817771
mikailaliyev <mikail.aliyev@gmail.com>
2023-03-29 21:53:40 +0400

    added: Tasks in README

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
 # IP Detector App
 This app detects your ip and shows additional information about your location and provider.
 
+---
+### Tasks
+
+- [ ] Show ip, location and other information to user
+- [ ] Add light/dark themes
 
 ##### Acknowledgements
 All data fetched through https://ipapi.co/ API
--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <div className="App">
-      
+      <h1>Hello</h1>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
