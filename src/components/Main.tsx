3cb5d31423ae415319831454c92eac7f422a8633
mikailaliyev <mikail.aliyev@gmail.com>
2023-03-30 01:20:43 +0400

    added: main fetch logic with workaround related to AdBlock extension blocking API calls

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,5 +1,22 @@
+import { useEffect, useState } from "react";
+
 export const Main = () => {
-  return (
-    <div>Main</div>
-  )
-}
+  const [data, setData] = useState({});
+
+  const handleData = async () => {
+    try {
+      const response = await fetch(import.meta.env.VITE_APP_API);
+      const result = await response.json();
+      console.log(result);
+      setData(result);
+    } catch (error) {
+      console.log(error);
+      alert("Turn off AdBlock to use the site\nthere is no adds here ☺");
+    }
+  };
+
+  useEffect(() => {
+    handleData();
+  }, []);
+  return <div className="main-content">{data.city}</div>;
+};
