8c8296e6a7632cb7aa56cabbffb5dfb2602cd2ca
mikailaliyev <mikail.aliyev@gmail.com>
2023-03-30 22:45:12 +0400

    added: footer image and link to GitHub

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,9 @@
 export const Footer = () => {
   return (
-    <div className="footer">Footer</div>
-  )
-}
+    <div className="footer">
+      <a href="https://github.com/mikailaliyev">
+        <img src="/GitHub-logo.png" alt="github-logo" className="github-logo" />
+      </a>
+    </div>
+  );
+};
