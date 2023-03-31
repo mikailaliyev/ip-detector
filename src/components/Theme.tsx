00690257472c574e63fb489c1d306d3dedc5adad
mikailaliyev <mikail.aliyev@gmail.com>
2023-03-31 23:55:15 +0400

    updates

--- src/components/Theme.tsx
+++ src/components/Theme.tsx
@@ -1,10 +1,15 @@
 import { useState } from "react";
 
 export const Theme = () => {
-  const [switcher, setSwitcher] = useState("light");
+  const [switcher, setSwitcher] = useState("dark");
+  const handleTheme = () => {
+    
+  }
   return (
     <div className="theme-switcher">
-      <button>{switcher}</button>
+      <button style={{ fontSize: "1rem", letterSpacing: "2px" }}>
+        {switcher}
+      </button>
     </div>
   );
 };
