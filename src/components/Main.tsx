6564da471f86de202cbed6c5ec191256d4d0aa13
mikailaliyev <mikail.aliyev@gmail.com>
2023-04-01 20:07:52 +0400

    update: inforamtion about page usage now located in sessionStorage instead of localStorage to prevent false results

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 
 export const Main = () => {
   const [data, setData] = useState<any>({});
-  const getData:any = localStorage.getItem("true")
+  const getData:any = sessionStorage.getItem("true")
   console.log(JSON.parse(getData))
 
   const handleData = async () => {
@@ -11,7 +11,7 @@ export const Main = () => {
       const result = await response.json();
       console.log(result);
       setData(result);
-      localStorage.setItem("true",  JSON.stringify(result))
+      sessionStorage.setItem("true", JSON.stringify(result));
     } catch (error) {
       console.log(error);
       alert("Turn off AdBlock to use the site\nthere is no adds here ☺");
@@ -19,17 +19,21 @@ export const Main = () => {
   };
 
   useEffect(() => {
-    if(!localStorage.getItem("true")) {
+    if (!sessionStorage.getItem("true")) {
       handleData();
-      console.log('data fetching')
-      return
-    }    
-    setData(JSON.parse(getData))
+      console.log("data fetching");
+      return;
+    }
+    setData(JSON.parse(getData));
   }, []);
-  return <div className="main-content">
-    <h1>Your ip: {data.ip}</h1>
-    <h2>IP version: {data.version}</h2>
-    <h2>Provider: {data.org}</h2>
-    <h2>Location: {data.city}, {data.country_name}</h2>
-    </div>;
+  return (
+    <div className="main-content">
+      <h1>Your ip: {data.ip}</h1>
+      <h2>IP version: {data.version}</h2>
+      <h2>Provider: {data.org}</h2>
+      <h2>
+        Location: {data.city}, {data.country_name}
+      </h2>
+    </div>
+  );
 };
