74e353b477bf34977041d740631dcb2e5f08508a
Mikayil <mikail.aliyev@gmail.com>
2023-04-07 10:20:36 +0400

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -8,8 +8,8 @@ import "./index.css";
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <BrowserRouter>
     <Routes>
-      <Route path="/" element={<App />} />
-      <Route path="/error" element={<Error />} />
+<!--       <Route path="/" element={<App />} />
+      <Route path="/error" element={<Error />} /> -->
       <Route path="/*" element={<App />} />
     </Routes>
   </BrowserRouter>
